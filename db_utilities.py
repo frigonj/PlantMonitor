b0db9fe88d480d6efde571fce40a57fe92593dd6
Jonathan Frigon <frigonjonathan@gmail.com>
2026-01-22 21:47:22 -0500

    moving towards init then get readings

--- db_utilities.py
+++ db_utilities.py
@@ -41,7 +41,7 @@ def add_reading(temp, hum, soil_moisture):
                      (datetime.now(), temp, hum, soil_moisture))
         conn.commit()
 
-def get_reading()
+def get_reading():
     """Gets the most recent readings available"""
     with sqlite3.connect(DB_FILE) as conn:
         result = conn.execute("SELECT temp, humidity, soil_moisture FROM readings ORDER BY timestamp DESC LIMIT 1")
