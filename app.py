972f51825891a7b8a6d9e46773ef07d69e563896
Jonathan Frigon <frigonjonathan@gmail.com>
2026-01-23 01:38:46 -0500

    sensor readings troubleshooting & fixing

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def set_state():
     db.update_plant_state(request.form['plant_state'])
     return redirect('/')
 
-@app.route("/")
+@app.route("/manual_update")
 def manual_update():
     sens.init_sens()
     return redirect("/")
