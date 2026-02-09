b0f188e52c4c4de7ed9eb8d7ab848d0a5b493efd
Jonathan Frigon <frigonjonathan@gmail.com>
2026-02-09 10:06:23 -0500

    Fixing time zone bugs

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,24 +1,32 @@
 let tempChart, humChart, soilChart;
 
+
+
 function loadCharts() {
     const range = document.getElementById("rangeSelect").value;
     fetch(`/api/history?range=${range}`)
-    .then(res => res.json())
-    .then(data => {
+        .then(res => res.json())
+        .then(data => {
 
-        const labels = data.timestamps.map(t =>
-            new Date(t).toLocaleTimeString()
-        );
+            const labels = data.timestamps.map(t => {
+                const date = new Date(t + ' UTC');
+                return date.toLocaleTimeString('en-US', {
+                    timeZone: 'America/New_York',
+                    hour: 'numeric',
+                    minute: '2-digit'
+                });
+            });
 
-        if (tempChart) tempChart.destroy();
-        if (humChart) humChart.destroy();
-        if (soilChart) soilChart.destroy();
 
-        tempChart = createChart("tempChart", labels, data.temperature, "Temperature °F");
-        humChart = createChart("humChart", labels, data.humidity, "Humidity %");
-        soilChart = createChart("soilChart", labels, data.soil, "Soil Moisture");
-        updateRangeLabel();
-    });
+            if (tempChart) tempChart.destroy();
+            if (humChart) humChart.destroy();
+            if (soilChart) soilChart.destroy();
+
+            tempChart = createChart("tempChart", labels, data.temperature, "Temperature °F");
+            humChart = createChart("humChart", labels, data.humidity, "Humidity %");
+            soilChart = createChart("soilChart", labels, data.soil, "Soil Moisture");
+            updateRangeLabel();
+        });
 }
 
 function createChart(canvasId, labels, values, label) {
